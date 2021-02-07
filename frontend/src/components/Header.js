02c48eeab246f64f3794c3be2422ccf37f032870
itzomen <peng.akebuon2468@gmail.com>
2021-02-07 20:51:44 +0100

    created navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Navbar, Nav, Container, Row} from 'react-bootstrap'
 
 function Header() {
     return (
-        <div>
-            <header>Header</header>
-        </div>
+        <header>
+        <Navbar bg="light" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">SaleBay</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/cart"><i className="fas fa-shopping-bag"></i> CART</Nav.Link>
+                        <Nav.Link href="/login"><i className="fas fa-sign-in-alt"></i> LOGIN</Nav.Link>
+                    
+                    </Nav>
+                
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </header>
     )
 }
 
