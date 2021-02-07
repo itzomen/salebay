d5d21e9f4b6aa5218fcd84a3dfdaad52d8992158
itzomen <peng.akebuon2468@gmail.com>
2021-02-07 20:08:53 +0100

    created Header and Footer. Added Cygorg bootstrap.min.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      Salebay
+    <div>
+      <Header />
+      <main>
+        <h1>Shop Here</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
