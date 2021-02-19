e67de65c38cc04722fc20344f253aa9866ed8745
itzomen <peng.akebuon2468@gmail.com>
2021-02-19 09:32:30 +0100

    created productReducer, productActions and added dispatch in Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,35 +1,43 @@
 import React, { useState, useEffect } from 'react'
+
+import { useDispatch, useSelector } from "react-redux";
+
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from "../actions/productActions";
+import { productListReducer } from '../reducers/productReducers';
 
 
 function Home() {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    //selecting var form productReducer which has error, loading, products
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList
 
     useEffect(() => {
-
-        async function fetchProducts(){
-            const { data } = await axios.get(`/api/products/`)
-            setProducts(data)
-            console.log('Data Fetced')
-        }
-
-        fetchProducts()
+        dispatch(listProducts())
 
     }, [])
+
 
     return (
         <div>
             <h2>Trending products</h2>
-            <Row>
-                {products.map(product => (
-                    // add key as each product should have unique id
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+
+            {loading ? <h2>Loading</h2>
+                : error ? <h3>{error}</h3>
+                : 
+                <Row>
+                    {products.map(product => (
+                        // add key as each product should have unique id
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+        }
+
+            
         </div>
     )
 }
