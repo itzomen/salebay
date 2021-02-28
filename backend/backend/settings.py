3f53143f07c4172f96cc949c29665e5d637a6c7a
itzomen <peng.akebuon2468@gmail.com>
2021-02-28 17:12:37 +0100

    installed simple jwt and configured

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,44 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
 ]
 
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
+
 MIDDLEWARE = [
     #for django cor headers
     'corsheaders.middleware.CorsMiddleware',
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
 
+# for simple jwt
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
+
 
 
 urlpatterns = [
+    path('users/login', TokenObtainPairView.as_view(), 
+                            name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getProduct, name="product"),
