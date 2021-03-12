49a14daceb8e366b35edd12b99a9010a5709ec56
itzomen <peng.akebuon2468@gmail.com>
2021-03-12 20:29:15 +0100

    added HashRouter to fixed Reload issues when serve with django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Header from './components/Header'
 import Footer from './components/Footer'
